585842e5d8baf60625086279acccf2c24b47949f
hoanprond123 <hoanprond121999@gmail.com>
2022-03-27 13:31:37 +0700

    new commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React,{ useState} from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import About from './pages/About'
-import Menu from './pages/Menu'
-import Stuff from './pages/Stuff'
-import Reservation from './pages/Reservation'
-import Gallery from './pages/Gallery'
-import Blog from './pages/Blog'
-import BlogSingle from './pages/BlogSingle'
-import Navbar from './components/Navbar'
-import data from './components/dataBlog'
-import SignUp from './pages/SignUp'
-import Login from './pages/Login'
-import DashBoard from './pages/DashBoard'
-import PrivateRoute from './components/PrivateRoute'
-import ForgotPassword from './pages/ForgotPassword';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Menu from "./pages/Menu";
+import Stuff from "./pages/Stuff";
+import Reservation from "./pages/Reservation";
+import Gallery from "./pages/Gallery";
+import Blog from "./pages/Blog";
+import BlogSingle from "./pages/BlogSingle";
+import Navbar from "./components/Navbar";
+import data from "./components/dataBlog";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import DashBoard from "./pages/DashBoard";
+import PrivateRoute from "./components/PrivateRoute";
+import ForgotPassword from "./pages/ForgotPassword";
+import ScrollToTop from "./ScrollToTop";
 function App() {
-
-
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/menu" element={<Menu />} />
         <Route path="/about" element={<About />} />
@@ -32,12 +32,18 @@ function App() {
         <Route path="/blog-single" element={<BlogSingle />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/log-in" element={<Login />} />
-        <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/dashboard" element={<DashBoard />} />
       </Routes>
     </Router>
-    
   );
 }
 
